ab00d9d4a198b244ed2d2dc568c4973ecda70ddf
hungmch95 <proxvalkryne@gmail.com>
2018-04-19 17:14:56 -0700

    fixed <<<Head things

--- MapReduce/MapReduceInterface.java
+++ MapReduce/MapReduceInterface.java
@@ -1,11 +1,8 @@
 import java.io.IOException;
 
 public interface MapReduceInterface {
-<<<<<<< HEAD
-	public void map(Long key, String value, Context context) throws IOException;
-	public void reduce(Long key, String[] value, Context context) throws IOException;
-=======
-	public void map(Long key, String value) throws IOException;
-	public void reduce(Long key, String value[]) throws IOException;
->>>>>>> 18c4b399a49b912c49feca840020a63b47f7404a
+
+	public void map(long key, String value, Context context) throws IOException;
+	public void reduce(long key, String[] value, Context context) throws IOException;
+
 }
